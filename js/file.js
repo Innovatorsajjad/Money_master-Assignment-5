ce9d65001d369d58adb679e7da009074b9068797
Khandaker Sajjad Hossen <sk.sajjad4207@gmail.com>
2022-02-17 18:15:38 +0600

    tag Names are selected js

---
+++ js/file.js
@@ -0,0 +1,13 @@
+ //All Tag Nems are selected
+ 
+ const IncomeTotalInputField = document.getElementById("Income_total_input");
+ const FoodCostInputField = document.getElementById("Food_cost_input");
+ const RentCostInputField = document.getElementById("Rent_cost_input");
+ const ClothesCostInputField=document.getElementById("Clothes_cost_input");
+ const CalculateAllButton=document.getElementById("calculate_Button");
+ const SavingsPersantageInputField=document.getElementById("Savings_persentage_input");
+ const CalculatePersantageButton=document.getElementById("calculate_Button");
+
+
+ //Error Handaler.........................
+ 
